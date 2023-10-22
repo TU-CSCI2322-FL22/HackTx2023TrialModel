9ceab250aa076434c175bdec658224d221999dd0
Q <q@Qs-MacBook-Air.local>
2023-10-22 04:04:15 -0500

    better ui

---
+++ pages/ℹ️About_Us.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+
+import streamlit as st
+
+
+st.title("About Us ℹ️")
+
+st.header("Inpiration")
+st.write("""
+In my daily life, whether at a cafe or walking in the park, I kept hearing curious questions about AI art. "Who's behind this artwork?" "Where did the AI pull these images from?" It seemed like everywhere I went, people were buzzing with wonder about the origins of AI-generated pieces.
+
+Every time I heard these questions, an idea started to form in the back of my mind. "What if there was a clear way to answer all these questions?" I thought about a tool that could not only craft captivating visuals but also trace back to their roots.
+
+With the energy and enthusiasm of these everyday conversations, I decided to turn this idea into reality for HackTX2023. A tool that demystifies AI art, making it transparent and relatable for all.
+""")
+
+# st.header("Our Team")
+# st.write("""
+# Introduce your team here. You might want to include your names, roles, 
+# backgrounds, or any other relevant details.
+# """)
+
+# st.header("Contact")
+# st.write("""
+# Provide contact information or instructions here. You might want to include 
+# an email address, a contact form, or links to social media profiles.
+# """)
+
+# st.header("Acknowledgements")
+# st.write("""
+# Acknowledge the contributions of others, thank anyone who helped, 
+# or provide references or links to resources you used.
+# """)
+
