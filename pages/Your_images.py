500a8663b2546d9e376e81a6dae348d64a0e27cd
Q <q@Qs-MacBook-Air.local>
2023-10-21 21:47:52 -0500

    Init

--- pages/Your_images.py
+++ pages/Your_images.py
@@ -1,11 +1,3 @@
 import streamlit as st
 
 
-def main():
-    pass
-
-if __name__ == "__main__":
-    if not st.session_state["authentication_status"]:
-        st.warning('Please login first')
-    else:
-        main()
